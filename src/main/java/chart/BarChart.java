a31b121ac21c946c16af56380aa4fec9085e48ad
Koodeu <limiera@gmail.com>
2021-09-26 13:25:28 +0200

    pierwszy commit

--- src/main/java/chart/BarChart.java
+++ src/main/java/chart/BarChart.java
@@ -121,4 +121,106 @@ public class BarChart {
     public void setDisplayLegend(boolean displayLegend) {
         this.displayLegend = displayLegend;
     }
+
+
+    public static class BarChartBuilder {
+
+
+        private String title;
+        private String fontName;
+        private int fontSize;
+        private String fontColor;
+        private int width;
+        private int height;
+        private String backgroundColor;
+        private List<String> data;
+        private boolean displayTitle;
+        private boolean displayLegend;
+
+
+        public BarChartBuilder withTitle(String title) {
+            this.title = title;
+            return this;
+
+        }
+
+        public BarChartBuilder withFontName(String fontName) {
+            this.fontName = fontName;
+            return this;
+        }
+
+        public BarChartBuilder withFontSize(int fontSize) {
+            this.fontSize = fontSize;
+            return this;
+        }
+
+        public BarChartBuilder withFontColor(String fontColor) {
+            this.fontColor = fontColor;
+            return this;
+        }
+
+        public BarChartBuilder withWidth(int width) {
+            this.width = width;
+            return this;
+        }
+
+        public BarChartBuilder height(int height) {
+            this.height = height;
+            return this;
+        }
+
+        public BarChartBuilder withBackGroundColor(String backgroundColor) {
+            this.backgroundColor = backgroundColor;
+            return this;
+        }
+
+        public BarChartBuilder withData(List<String> data) {
+            this.data = data;
+            return this;
+        }
+
+        public BarChartBuilder withDisplayTitle(boolean displayTitle) {
+            this.displayTitle = displayTitle;
+            return this;
+        }
+
+        public BarChartBuilder withDisplayLegend(boolean displayLegend) {
+            this.displayLegend = displayLegend;
+            return this;
+        }
+
+        public BarChartBuilder build() {
+            BarChart barChart = new BarChart();
+            barChart.title = this.title;
+            barChart.fontName = this.fontName;
+            barChart.fontSize = this.fontSize;
+            barChart.fontColor = this.fontColor;
+            barChart.width = this.width;
+            barChart.height = this.height;
+            barChart.backgroundColor = this.backgroundColor;
+            barChart.displayLegend = this.displayLegend;
+            barChart.displayTitle = this.displayTitle;
+            barChart.data = this.data;
+            return barChart;
+        }
+
+        @Override
+        public String toString() {
+            return "BarChartBuilder{" +
+                    "title='" + title + '\'' +
+                    ", fontName='" + fontName + '\'' +
+                    ", fontSize=" + fontSize +
+                    ", fontColor='" + fontColor + '\'' +
+                    ", width=" + width +
+                    ", height=" + height +
+                    ", backgroundColor='" + backgroundColor + '\'' +
+                    ", data=" + data +
+                    ", displayTitle=" + displayTitle +
+                    ", displayLegend=" + displayLegend +
+                    ", with=" + with +
+                    ", with=" + with +
+                    '}';
+        }
+    }
+
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,5 +7,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         BarChart barChart = new BarChart("Task types report",new ArrayList());
+
+
+        BarChart barChart1 = BarChart.BarChartBuilde
+
+
     }
+
 }
